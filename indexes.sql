d401875b53ff25b2ca5df4d64cc9e80f4327aace
joaqim-uva <joaquintp05@gmail.com>
2026-05-29 12:40:41 -0400

    cambio ubicacion indice matview

--- indexes.sql
+++ indexes.sql
@@ -1,7 +1,7 @@
 DROP INDEX IF EXISTS id_orders_order_date;
 DROP INDEX IF EXISTS idx_email_users;
 DROP INDEX IF EXISTS idx_reviews_bid_revdate;
-
+DROP INDEX IF EXISTS o_date_matview_idx;
 
 -- Índice N°1 
 CREATE INDEX id_orders_order_date ON orders(order_date DESC);
@@ -12,5 +12,5 @@ CREATE INDEX idx_email_users on users(LOWER(email));
 -- Índice N°3 
 CREATE INDEX idx_reviews_bid_revdate on reviews(book_id, review_date DESC);
 
-
-
+-- Índice N°4 (sobre matview)
+CREATE INDEX o_date_matview_idx ON orders_items_books(order_date);
--- matview.sql
+++ matview.sql
@@ -1,4 +1,3 @@
-DROP INDEX IF EXISTS o_date_matview_idx;
 DROP MATERIALIZED VIEW IF EXISTS orders_items_books;
 
 --Crear MV
@@ -7,7 +6,4 @@ SELECT oi.quantity, oi.unit_price, oi.book_id,
 b.genre, b.title, o.order_date, o.user_id
 FROM orders o 
 JOIN order_items oi ON oi.order_id = o.order_id
-JOIN books b ON b.book_id = oi.book_id; 
-
--- Índice N°4 (sobre matview)
-CREATE INDEX o_date_matview_idx ON orders_items_books(order_date);
+JOIN books b ON b.book_id = oi.book_id;
