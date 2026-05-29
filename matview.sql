7a61b6e58708f48228c3048a1bf00348de94261a
Sofía Guzmán <sofiaguzmandls@estudiante.uc.cl>
2026-05-28 23:46:55 -0400

    agregué un comentario

--- matview.sql
+++ matview.sql
@@ -4,4 +4,6 @@ b.genre, b.title, o.order_date, o.user_id
 FROM orders o 
 JOIN order_items oi ON oi.order_id = o.order_id
 JOIN books b ON b.book_id = oi.book_id; 
+
+-- Índice N°4 (sobre matview)
 CREATE INDEX o_date_matview_idx ON orders_items_books (order_date);
