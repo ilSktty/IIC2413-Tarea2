b0b4a4cdbf836abe0427c1d38456f1ffe3014faa
joaqim-uva <joaquintp05@gmail.com>
2026-05-28 22:50:59 -0400

    matview index

--- matview.sql
+++ matview.sql
@@ -3,4 +3,5 @@ SELECT oi.quantity, oi.unit_price, oi.book_id,
 b.genre, b.title, o.order_date, o.user_id
 FROM orders o 
 JOIN order_items oi ON oi.order_id = o.order_id
-JOIN books b ON b.book_id = oi.book_id;
+JOIN books b ON b.book_id = oi.book_id; 
+CREATE INDEX o_date_matview_idx ON orders_items_books (order_date);
